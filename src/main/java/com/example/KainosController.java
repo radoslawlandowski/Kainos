27c22652b4840fef06a5d6422dd3e1b64bce3eb8
Radosław Landowski <radoslaw.landowski@gmail.com>
2016-05-22 12:33:08 +0200

    #5 Added class for computing income

--- src/main/java/com/example/KainosController.java
+++ src/main/java/com/example/KainosController.java
@@ -1,10 +1,16 @@
 package com.example;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.utils.MoneyCalculator;
 
 @RestController
 public class KainosController {
@@ -22,7 +28,12 @@ public class KainosController {
 		String path = "/home/radek/Documents/newWorkspace/Kainos/src/main/resources/data.csv";
 		
     	service.insertDataFromFile(path);
-        return service.selectWhereDataMatches("1998-01-05", "1998-05-19").toString();
+        
+    	MoneyCalculator calc = new MoneyCalculator();
+    	List<ArrayList<String>> result = calc.calculateIncome(service.selectWhereDataMatches("1998-01-05", "1998-01-23"), new BigDecimal(10000));
+    	List<ArrayList<String>> sResult = calc.calculateIncomeConstantPercentage(service.selectWhereDataMatches("1998-01-05", "1998-01-23"), new BigDecimal(10000), new BigDecimal(110));
+    	
+    	return sResult.toString();
     }
 
 }
--- src/main/java/com/example/utils/MoneyCalculator.java
+++ src/main/java/com/example/utils/MoneyCalculator.java
@@ -1,14 +1,61 @@
 package com.example.utils;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MoneyCalculator {
+
+	public static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
+
 	public MoneyCalculator() {
-		
+	 
 	}
 	
-	public List<ArrayList<String>> calculateIncome(List<ArrayList<String>> data) {
-		
+	private BigDecimal calculateSingleIncome(BigDecimal inputValue, BigDecimal percentage) {
+		BigDecimal val = inputValue.multiply(percentage).divide(ONE_HUNDRED);
+		return val;
 	}
+	
+	public List<ArrayList<String>> calculateIncome(List<ArrayList<String>> data, BigDecimal inputValue) {
+		
+		List<ArrayList<String>> outputList = new ArrayList<ArrayList<String>>();
+		
+		int listSize = data.size();
+		for(int i = 0 ; i < listSize ; i++) {
+			String v = data.get(i).get(1);
+			v = v.replaceAll("\\s+",""); // remove whitespaces
+			BigDecimal val = new BigDecimal(v);
+			BigDecimal percentage = val;
+			BigDecimal outputValue = calculateSingleIncome(inputValue, percentage);
+			outputValue = outputValue.setScale(2, RoundingMode.HALF_EVEN);
+			inputValue = outputValue;
+			
+			ArrayList<String> row = new ArrayList<>();
+			row.add(data.get(i).get(0));
+			row.add(outputValue.toString());
+			outputList.add(row);
+		}
+		return outputList;
+	}
+	
+	public List<ArrayList<String>> calculateIncomeConstantPercentage(List<ArrayList<String>> data, BigDecimal inputValue, BigDecimal percentage) {
+		
+		List<ArrayList<String>> outputList = new ArrayList<ArrayList<String>>();
+		
+		int listSize = data.size();
+		for(int i = 0 ; i < listSize ; i++) {
+			BigDecimal outputValue = calculateSingleIncome(inputValue, percentage);
+			outputValue = outputValue.setScale(2, RoundingMode.HALF_EVEN);
+			inputValue = outputValue;
+			ArrayList<String> row = new ArrayList<>();
+			row.add(data.get(i).get(0));
+			row.add(outputValue.toString());
+			outputList.add(row);
+		}
+		return outputList;
+
+	}
+	
 }
